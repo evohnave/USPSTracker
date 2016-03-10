17523fafe62f0a5518feac1faf73ea344d33933a
evohnave <eric.vanhove@gmail.com>
2016-03-10 08:25:38 -0500

    Added tracking number

--- TrackerScript.R
+++ TrackerScript.R
@@ -2,7 +2,8 @@ trackMyPackages <- function(){
     # Place the tracking numbers in trackIDs here...
     trackIDs <- c("RA548921599DE",
                   "RN262336226GB",
-                  "RA128783708SI")
+                  "RA128783708SI",
+                  "RB782044557DE")
 
     source(file = "trackPackage.R")
     lapply(X = trackIDs, FUN = trackPackage)
