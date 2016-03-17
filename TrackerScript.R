da456a114346f78a37e180c9b556407cf281c11c
evohnave <eric.vanhove@gmail.com>
2016-03-17 11:00:25 -0400

    Added tracking ID's

--- TrackerScript.R
+++ TrackerScript.R
@@ -3,11 +3,13 @@
 # "RG011796925DE" - Arab Byzantine Book
 # "RN262336226GB" - Roma coin
 # "RA128783708SI" - 5 1 kurus
+# "RA471329095DE" - Incl Bafa
 
 trackMyPackages <- function(){
     # Place the tracking numbers in trackIDs here...
     trackIDs <- c("RB782044557DE",
-                  "RA128783708SI")
+                  "RA128783708SI",
+                  "RA471329095DE")
 
     source(file = "trackPackage.R")
     WhereAreThey <- as.data.frame(lapply(X = trackIDs, FUN = trackPackage),
