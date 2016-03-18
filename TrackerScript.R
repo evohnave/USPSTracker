29f56845315c07d604505397222a0a9f245116b2
evohnave <eric.vanhove@gmail.com>
2016-03-18 16:00:44 -0400

    Update Tracking IDs

--- TrackerScript.R
+++ TrackerScript.R
@@ -4,11 +4,11 @@
 # "RN262336226GB" - Roma coin
 # "RA128783708SI" - 5 1 kurus
 # "RA471329095DE" - Incl Bafa
+# "RB782044557DE" - Savoca 3 lot package
 
 trackMyPackages <- function(){
     # Place the tracking numbers in trackIDs here...
-    trackIDs <- c("RB782044557DE",
-                  "RA128783708SI",
+    trackIDs <- c("RA128783708SI",
                   "RA471329095DE")
 
     source(file = "trackPackage.R")
