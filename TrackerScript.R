41d9cd3daf1ed649c896b7993bf9bbe9097c3b26
evohnave <eric.vanhove@gmail.com>
2016-04-14 16:37:02 -0400

    changes to tracked id's

--- TrackerScript.R
+++ TrackerScript.R
@@ -1,21 +1,21 @@
-# Delivered packages
-# "RA548921599DE",
-# "RG011796925DE" - Arab Byzantine Book
-# "RN262336226GB" - Roma coin
+#  Packages
 # "RA128783708SI" - 5 1 kurus
-# "RA471329095DE" - Incl Bafa - Rcvd
+# "RA471329095DE" - Incl Bafa - received
+# "RA548921599DE"
+# "RB033236168DE" - Solidus 2 lots - Delivered
+# "RB516620411DE" - Arab Byzantine coin - received
 # "RB782044557DE" - Savoca 3 lot package
 # "RB882050630DE" - Savoca package
-# "RB033236168DE" - Solidus 2 lots
-# "RN256013306GB" - Naville Arab-Byzantine
+# "RG011796925DE" - Arab Byzantine Book
+# "RN256013306GB" - Naville Arab-Byzantine - received
+# "RN262336226GB" - Roma coin
+# "RR1815899110AL" - 1293/33 2k
 
 trackMyPackages <- function(){
     # Place the tracking numbers in trackIDs here...
-    trackIDs <- c("RA128783708SI",
-                  "RB882050630DE",
+    trackIDs <- c("RB882050630DE",
                   "RN262336226GB",
-                  "RB033236168DE",
-                  "RN256013306GB")
+                  "RR1815899110AL")
 
     source(file = "trackPackage.R")
     WhereAreThey <- as.data.frame(lapply(X = trackIDs, FUN = trackPackage),
